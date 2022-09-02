9b19708ac7d286b482e5e946b4152b79e74cd6c0
Your Name <naimur202163@gmail.com>
2022-09-02 14:20:13 +0600

    sample added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
-import "./App.css";
-import toogle from "./hooks/toogle";
-function App() {
-  const [isOn, setIsOn] = toogle()
-  return (
-    <div className="App">
-      <h2>Naimur Rahman</h2>
+import React from "react";
 
-      <button>is active{`${isOn}`}</button>
-    </div>
-  );
+export default function App() {
+  return <div>This is Naimur Rahman D</div>;
 }
-
-export default App;
--- src/hooks/toogle.js
+++
@@ -1,7 +0,0 @@
-import React, { useState } from "react";
-
-export default function toogle(active = false) {
-  const [isActive, setIsActive] = useState(active);
-  console.log({active});
-  return [isActive,setIsActive]
-}
